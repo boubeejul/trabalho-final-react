ff8694b068399d28015fd0681944826fc3fbe166
João Marco Piccoli Cardoso <127253938+JoaoMarcoPiccoliCardoso@users.noreply.github.com>
2023-06-08 20:39:42 -0300

    Primeiro carosel na Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,41 @@
 import { Header } from "../../global/components/Header"
 import { Menu } from "../../global/components/Menu"
+import { Images } from "./component/carousel"
+import { Container, SliderContainer, Text, Hr } from "./style"
+import { SlideCard } from "./component/ProductCarousel"
 
 
 export function Home() {
     return (
         <>
         <Header></Header>
-        <Menu></Menu>
-        {/* <Slide></Slide>
+        <Menu></Menu>      
+        <Images/>
 
         <Container>
-            <Info></Info>
-            <SlideCard></SlideCard>
-            <SlideCard></SlideCard>
-            <SlideCard></SlideCard>
-            <Publi>
+            {/* <Info></Info> */}
+            <SliderContainer>
+                <Text>Mais Vendidas</Text>
+                <Hr/>
+                <SlideCard/>
+            </SliderContainer>
+            <SliderContainer>
+                <Text>Mais Procuradas</Text>
+                <Hr/>
+                <SlideCard/>
+            </SliderContainer>
+            <SliderContainer>
+                <Text>Em Promoção</Text>
+                <Hr/>
+                <SlideCard/>
+            </SliderContainer>
+            {/* <Publi>
                 <Promo></Promo>
                 <Promo></Promo>
-            </Publi>
+            </Publi> */}
         </Container>
 
-        <Footer></Footer> */}
+        {/* <Footer></Footer> */}
         </>
     )
 }
---
+++ src/pages/Home/component/ProductCarousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function SlideCard() {
+  return (
+    <div>
+        <img src="https://m.media-amazon.com/images/I/61AGZ37D7eL.jpg" alt="imagemCarta" />
+        <img src="https://m.media-amazon.com/images/I/61AGZ37D7eL.jpg" alt="imagemCarta" />
+        <img src="https://m.media-amazon.com/images/I/61AGZ37D7eL.jpg" alt="imagemCarta" />
+        <img src="https://m.media-amazon.com/images/I/61AGZ37D7eL.jpg" alt="imagemCarta" />
+        <img src="https://m.media-amazon.com/images/I/61AGZ37D7eL.jpg" alt="imagemCarta" />
+    </div>
+  );
+}
